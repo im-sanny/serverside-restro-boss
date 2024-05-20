4ae8d01ad73b1a3fd804a98bba595f53fab6410f
im-sanny <raselparvezsanny@gmail.com>
2024-05-20 18:31:43 +0600

    added delete api

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 9000;
 
-
-//middleware 
-app.use(cors())
-app.use(express.json())
-
-
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -19,38 +16,58 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const menuCollection = client.db('restro-boss').collection('menu')
-    const reviewsCollection = client.db('restro-boss').collection('reviews')
+    const menuCollection = client.db("restro-boss").collection("menu");
+    const reviewsCollection = client.db("restro-boss").collection("reviews");
+    const cartCollection = client.db("restro-boss").collection("carts");
 
-    app.get('/menu', async(req, res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result)
-    })
-    app.get('/review', async(req, res)=>{
-        const result = await reviewsCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/review", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
 
+    // carts collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) =>{
-    res.send('boss is crying')
-})
+app.get("/", (req, res) => {
+  res.send("boss is crying");
+});
 
 app.listen(port, () => {
-    console.log(`restro boss is running on port ${port}`);
-})
+  console.log(`restro boss is running on port ${port}`);
+});

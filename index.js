823221d5200e0034a9192a698f9991fce43b8b80
im-sanny <raselparvezsanny@gmail.com>
2024-05-20 11:32:32 +0600

    added review collection fetch method

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require('dotenv').config()
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 9000;
 
 
 //middleware 
@@ -14,7 +14,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,10 +25,14 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const menuCollection = client.db('restro-boss').collection('menu')
-
+    const reviewsCollection = client.db('restro-boss').collection('reviews')
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
+        res.send(result)
+    })
+    app.get('/review', async(req, res)=>{
+        const result = await reviewsCollection.find().toArray();
         res.send(result)
     })
 

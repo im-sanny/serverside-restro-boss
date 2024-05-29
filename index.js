ec5db285a7b7a9f29f2ba46d65107a5a5dfa9511
im-sanny <raselparvezsanny@gmail.com>
2024-05-29 10:21:28 +0600

    api created to delete data after successful payment

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     const menuCollection = client.db("restro-boss").collection("menu");
     const reviewsCollection = client.db("restro-boss").collection("reviews");
     const cartCollection = client.db("restro-boss").collection("carts");
+    const paymentCollection = client.db("restro-boss").collection("payments");
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -202,6 +203,21 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      //carefully delete each item from the card
+      console.log("payment info", payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
+      const deleteResults = await cartCollection.deleteMany(query)
+      res.send({paymentResult, deleteResults});
     });
 
     await client.db("admin").command({ ping: 1 });
